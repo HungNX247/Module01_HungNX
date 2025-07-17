d13c5939480414a3d8d5106acf9c1c729e9ee3a3
HungNX247 <hungnx6666@gmail.com>
2025-07-17 11:42:53 +0700

    1707251142

---
+++ Lesson7/Thuchanh/khaibaobien-baitap1_2.js
@@ -0,0 +1,24 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = "Hà Nội";
+
+document.writeln("Kết quả in ra bài tập 1:");
+document.writeln("</br>");
+document.writeln("i = " + i);
+document.writeln("</br>");
+document.writeln("f = " + f);
+document.writeln("</br>");
+document.writeln("b = " + b);
+document.writeln("</br>");
+document.writeln("s = " + s);
+document.writeln("</br>");
+document.writeln("</br>");
+
+let width = 20;
+let height = 10;
+let area = width * height;
+
+document.writeln("Kết quả in ra bài tập 2:");
+document.writeln("</br>");
+document.writeln("Area = " + area);
